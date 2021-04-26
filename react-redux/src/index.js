ea6da443000cda9acf4a12ad10cdc8c0eb00befc
Igor <igor.solovei1983@gmail.com>
2021-04-26 23:27:26 +0300

    updated

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,7 +1,7 @@
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import reducer from './reducer';
-import React from 'react'; 
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './style.css';
@@ -9,8 +9,8 @@ import './style.css';
 const store = createStore(reducer);
 
 ReactDOM.render(
-    <Provider store = {store}> 
-        <App/>
-    </Provider>
-    , document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>
+	, document.querySelector('#root')
 );
