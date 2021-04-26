772325ae4f8d19d53cda37d94d318f85ac183e90
Igor <igor.solovei1983@gmail.com>
2021-04-26 23:23:21 +0300

    updated

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,50 +1,16 @@
-import { createStore } from 'redux'
-
-const reducer = (state = 0, action) => {
-	switch (action.type) {
-		case 'INC':
-			return state + 1;
-		case 'DEC':
-			return state - 1;
-		case 'RES':
-			return 0;
-		default:
-			return state;
-	}
-}
-
-const inc = () => ({ type: 'INC' });
-const dec = () => ({ type: 'DEC' });
-const rnd = (value) => ({ type: 'RES', value });
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './style.css';
 
 const store = createStore(reducer);
 
-document.querySelector('#inc').addEventListener('click', () => {
-	store.dispatch(inc());
-});
-
-document.querySelector('#dec').addEventListener('click', () => {
-	store.dispatch(dec());
-});
-
-document.querySelector('#res').addEventListener('click', () => {
-
-	store.dispatch(rnd());
-});
-
-const update = () => {
-	document.querySelector('#counter').textContent = store.getState();
-}
-
-
-store.subscribe(update)
-
-
-
-// store.dispatch({ type: 'INC' });
-// store.dispatch({ type: 'INC' });
-// store.dispatch({ type: 'INC' });
-
-
-
-
+ReactDOM.render(
+    <Provider store = {store}> 
+        <App/>
+    </Provider>
+    , document.getElementById('root')
+);
